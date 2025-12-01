46f2f2f63f5ffa6b5ea6de27af3d3a3092806a79
Magdalena Bober <magdakochman7@gmail.com>
2025-12-01 18:52:18 +0100

    feat: day 1

--- day-1/index.ts
+++ day-1/index.ts
@@ -4,4 +4,24 @@ const filepath = new URL('day1.txt', import.meta.url);
 const data = await fs.promises.readFile(filepath, 'utf8');
 const dataArray = data.split('\n');
 
-// TODO: solve the puzzle
+// part 1
+let startPoint = 50;
+let correctPoints = 0;
+for (let item of dataArray) {
+  const turn = item.substring(0, 1);
+  const step = parseInt(item.substring(1), 10);
+
+  if (turn === 'R') {
+    startPoint += step;
+    startPoint %= 100;
+  } else if (turn === 'L') {
+    startPoint += 100 - step;
+    startPoint %= 100;
+  }
+
+  if (startPoint === 0) {
+    correctPoints++;
+  }
+}
+
+console.log('🚀 ~ correctPoints:', correctPoints);
