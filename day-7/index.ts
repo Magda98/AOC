dbfb2a1ad3792ca967092b79ad1287bad2b6e725
Magdalena Bober <magdakochman7@gmail.com>
2025-12-13 17:19:12 +0100

    feat: day-7 part 2

--- day-7/index.ts
+++ day-7/index.ts
@@ -1,22 +1,59 @@
 import fs from 'fs';
+import { sum } from 'lodash-es';
 
-const filepath = new URL('day7.txt', import.meta.url);
+const filepath = new URL('day7test.txt', import.meta.url);
 const data = await fs.promises.readFile(filepath, 'utf8');
 const dataArray = data.split('\n').map((line) => line.split(''));
 
-const tachyonsArray = dataArray.slice(1);
-const beams: boolean[] = dataArray[0].map((char) => char === 'S');
+console.log('Total beam splits: ', findTotalBeamSplits(dataArray));
+console.log('Total paths: ', findTotalPaths(dataArray));
 
-let beamSplits = 0;
-for (let [columnIndex, tachyons] of tachyonsArray.entries()) {
-  for (let [rowIndex, char] of tachyons.entries()) {
-    if (char === '^' && beams[rowIndex]) {
-      beamSplits++;
-      beams[rowIndex] = false;
-      beams[rowIndex - 1] = true;
-      beams[rowIndex + 1] = true;
+function findTotalBeamSplits(grid: string[][]): number {
+  const tachyonsArray = grid.slice(1);
+  const beams: boolean[] = grid[0].map((char) => char === 'S');
+  const cols = beams.length;
+  const rows = tachyonsArray.length;
+  let beamSplits = 0;
+
+  for (let rowIndex = 0; rowIndex < rows - 1; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < cols; columnIndex++) {
+      const nextTile = tachyonsArray[rowIndex + 1][columnIndex];
+      if (nextTile === '^' && beams[columnIndex]) {
+        beamSplits++;
+        beams[columnIndex] = false;
+        beams[columnIndex - 1] = true;
+        beams[columnIndex + 1] = true;
+      }
     }
   }
+
+  return beamSplits;
 }
 
-console.log('Total beam splits: ', beamSplits);
+function findTotalPaths(grid: string[][]): number {
+  const tachyonsArray = grid.slice(1);
+  const startCol = grid[0].indexOf('S');
+  const cols = grid[0].length;
+  const rows = tachyonsArray.length;
+
+  let currentBeams: number[] = new Array(cols).fill(0);
+  currentBeams[startCol] = 1;
+
+  for (let rowIndex = 0; rowIndex < rows - 1; rowIndex++) {
+    let nextBeams = new Array(cols).fill(0);
+
+    for (let columnIndex = 0; columnIndex < cols; columnIndex++) {
+      const beamCount = currentBeams[columnIndex];
+      if (beamCount === 0) continue;
+      const nextTile = tachyonsArray[rowIndex + 1][columnIndex];
+      if (nextTile === '^') {
+        if (columnIndex - 1 >= 0) nextBeams[columnIndex - 1] += beamCount;
+        if (columnIndex + 1 < cols) nextBeams[columnIndex + 1] += beamCount;
+      } else {
+        nextBeams[columnIndex] += beamCount;
+      }
+    }
+    currentBeams = nextBeams;
+  }
+  return sum(currentBeams);
+}
